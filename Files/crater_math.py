fa68a6f092ad65617be09e2ca135b413f553c72b
Isaac Caro <isaaccaro24@mail.fresnostate.edu>
2019-04-20 23:27:49 -0700

    added more calculation functions

--- Files/crater_math.py
+++ Files/crater_math.py
@@ -9,8 +9,37 @@ def solveDiameter(craterCF, gravAcc, gravAccSurface, kinEnergy, densityImpactor,
 		gravAccSurface = Acceleration at the surface of body on which crater is formed
 		kinEnergy = kinetic energy of impactor
 		densityImpactor = denity of impactor in g/ cubic cm
-		densitySurface = density of surface
+		densitySurface = density of surface also g / cubic cm
 	'''
-		diameter = .07* craterCF * pow((gravAcc/gravAccSurface), 0.1667) * pow((kinEnergy * (densityImpactor/densitySurface)), .2941)
+		densImp = densityImpactor/1000.00
+		densSurf = densitySurface/1000.00
+
+		diameter = .07* craterCF * pow((gravAcc/gravAccSurface), .1667) * pow((kinEnergy * (densImp/densSurf)), .2941)
 		return diameter
+
+def solveKinEnergy(mass, velocity):
+	''' based off of: http://keyah.asu.edu/lessons/MeteorCrater/KM13.html
+
+		mass = mass of metoerite in g to convert to kg
+		s = velocity of meteorite in meters/second (according to webpage avg speed of metoerites is 20k m/s)
+
+		kinEnergy = kinetic energy in joules
+
+	'''
+	massKG = mass/1000.00
+	
+	kinEnergy = (.5) * massKG * pow(velocity, 2)
+	return kinEnergy
+
+def getVolumeMet(density, mass):
+	''' based off of: http://keyah.asu.edu/lessons/MeteorCrater/KM13.html
+		
+		density = density of meteorite material in kg/m^3
+		mass = mass in grams
+		volume = volume of meteorite in m^3
+	'''
+	massKG = mass/1000.00
+
+	volume = massKG/density
+	return volume
 
--- Files/our_tools.py
+++ Files/our_tools.py
@@ -1,2 +1,5 @@
 #script for any tools we create for ourselves
 #i.e. functions, classes, etc.
+import pandas as pandas
+
+df=read_csv("Datasets/meteorite-landings.csv")
